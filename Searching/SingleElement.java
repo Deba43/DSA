394e098a8622e92caf6627ccbb509c6039f02c75
Debadatta Behera <debadatta016@gmail.com>
2024-08-31 18:05:52 +0530

    files

---
+++ Searching/SingleElement.java
@@ -0,0 +1,23 @@
+package Searching;
+
+public class SingleElement {
+    public static void main(String args[]) {
+        int nums[] = { 1, 1, 2, 3, 3, 4, 4, 8, 8 };
+        int result = singleNonDuplicate((nums));
+        System.out.println(result);
+
+    }
+
+    public static int singleNonDuplicate(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left = (right - left) / 2;
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return nums[left];
+    }
+
+}
