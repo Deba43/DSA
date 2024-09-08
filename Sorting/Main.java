2a87e3a50c375f3e39284c362d8d4b1a9d1683ae
Debadatta Behera <debadatta016@gmail.com>
2024-09-08 23:14:26 +0530

    files

--- Sorting/Main.java
+++ Sorting/Main.java
@@ -34,28 +34,18 @@ public class Main {
     }
 
     static void selection(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            // find the max item in the remaining array and swap with correct index
-            int last = arr.length - i - 1;
-            int maxIndex = getMaxIndex(arr, 0, last);
-            swap(arr, maxIndex, last);
-        }
-    }
-
-    static int getMaxIndex(int[] arr, int start, int end) {
-        int max = start;
-        for (int i = start; i <= end; i++) {
-            if (arr[max] < arr[i]) {
-                max = i;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
             }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
-        return max;
-    }
 
-    static void swap(int[] arr, int first, int second) {
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
     }
 
 }
