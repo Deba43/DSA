dd00ee15b10a9a004d2edde118046e0f8a933c36
Debadatta Behera <debadatta016@gmail.com>
2024-09-09 21:39:49 +0530

    files

---
+++ Sorting/FirstMissingPositive.java
@@ -0,0 +1,36 @@
+package Sorting;
+
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int arr[] = { -1, 1, 3, 4 };
+        System.out.println(firstMissingPositive(arr));
+
+    }
+
+    static int firstMissingPositive(int arr[]) {
+        int i = 0;
+        while (i < arr.length) {
+            int correctIndex = arr[i] - 1;
+            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index + 1) {
+                return index + 1;
+            }
+
+        }
+        return arr.length + 1;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+
+}
