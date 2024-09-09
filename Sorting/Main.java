4fe582f4c880abc99bf94a8df5e5dd92399c24a1
Debadatta Behera <debadatta016@gmail.com>
2024-09-09 11:46:49 +0530

    files

--- Sorting/Main.java
+++ Sorting/Main.java
@@ -9,6 +9,8 @@ public class Main {
         System.out.println(Arrays.toString(arr));
         selection(arr);
         System.out.println(Arrays.toString(arr));
+        insertion(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     static void bubble(int arr[]) {
@@ -46,6 +48,21 @@ public class Main {
             arr[i] = temp;
         }
 
+    }
+
+    static void insertion(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            // Move elements of array[0..i-1], that are greater than key,
+            // to one position ahead of their current position
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
     }
 
 }
