c75ddce9052bba4fc5cd8472edf0141ca340dcc9
Deba43 <debadatta016@gmail.com>
2024-10-09 23:14:25 +0530

    files

---
+++ String/ValidParentheses.java
@@ -0,0 +1,27 @@
+package String;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "({[]})";
+
+        boolean b = isValid(s);
+        System.out.println(b);
+    }
+
+    public static boolean isValid(String s) {
+        int i = -1;
+        char[] stack = new char[s.length()];
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[')
+                stack[++i] = ch;
+            else if (i >= 0 && ((stack[i] == '(' && ch == ')') || (stack[i] == '{' && ch == '}')
+                    || (stack[i] == '[' && ch == ']')))
+                i--;
+            else
+                return false;
+
+        }
+        return i == -1;
+    }
+
+}
