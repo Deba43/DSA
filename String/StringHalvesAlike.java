877f6e8381e78bb0b3a43c23273d74aee4f34885
Deba43 <debadatta016@gmail.com>
2024-10-14 23:25:48 +0530

    files

---
+++ String/StringHalvesAlike.java
@@ -0,0 +1,22 @@
+package String;
+
+import java.util.HashSet;
+
+public class StringHalvesAlike {
+    public boolean halvesAreAlike(String s) {
+        HashSet<Character> set = new HashSet<>();
+        for (char c : "aeiouAEIOU".toCharArray())
+            set.add(c);
+
+        int count = 0;
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (set.contains(s.charAt(i)))
+                count++;
+            if (set.contains(s.charAt(i + s.length() / 2)))
+                count--;
+        }
+
+        return count == 0;
+    }
+    
+}
