54b496b1b584711b04846a8436cdf25e28d8518b
Deba43 <debadatta016@gmail.com>
2024-10-17 18:15:43 +0530

    files

---
+++ String/Robot.java
@@ -0,0 +1,28 @@
+package String;
+
+public class Robot {
+    public static void main(String[] args) {
+        // String moves = "UD";
+        String moves = "LUDR";
+        boolean re = judgeCircle(moves);
+        System.out.println(re);
+
+    }
+
+    public static boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for (char ch : moves.toCharArray()) {
+            if (ch == 'U')
+                y++;
+            else if (ch == 'D')
+                y--;
+            else if (ch == 'R')
+                x++;
+            else if (ch == 'L')
+                x--;
+        }
+        return x == 0 && y == 0;
+    }
+
+}
